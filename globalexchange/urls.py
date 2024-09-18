422f6d75e2431b829af513cfb6af9058bb67a648
goldenjefempire <jeffemuodafe124@gmail.com>
2024-09-18 18:14:10 +0100

    Added Bootstrap for styling, implemented authentication views, and configured admin interface

--- globalexchange/urls.py
+++ globalexchange/urls.py
@@ -1,28 +1,12 @@
-"""
-URL configuration for globalexchange project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from globalxchange import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('globalxchange.urls')),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
@@ -33,4 +17,7 @@ urlpatterns = [
     path('place_order/', views.place_order, name='place_order'),
     path('order_history/', views.order_history, name='order_history'),
     path('search/', views.search_products, name='search_products'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- globalxchange/admin.py
+++ globalxchange/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from .models import Product, Order
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'created_at')
     search_fields = ('name', 'description')
 
-admin.site.register(Product, ProductAdmin)
-
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('user', 'created_at', 'status')
     list_filter = ('status',)
-
-admin.site.register(Order, OrderAdmin)
