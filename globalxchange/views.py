1efab6144d34e26aa01c40d69538833ce441aec1
goldenjefempire <jeffemuodafe124@gmail.com>
2024-09-19 14:21:56 +0100

    Added product listings, order placement, and order history features; updated URLs

--- globalxchange/views.py
+++ globalxchange/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
